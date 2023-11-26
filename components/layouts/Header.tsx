6a0e759d2bf1bd37e854e343b10ccce19d961101
SimoneGuarneri <simone.guarneri.s@gmail.com>
2023-11-26 22:45:26 +0100

    Header dark mode adapt

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -8,12 +8,12 @@ export const Header: FunctionComponent<HeaderProps> = ({ hideTagline }) => {
   return (
     <header className="header text-center flex flex-col p-0 md:p-10">
       {!hideTagline && (
-        <h2 className="subtitle text-left text-md md:text-xl text-baseDark">
+        <h2 className="pl-1 subtitle text-left text-md md:text-xl text-baseDark dark:text-baseWhite">
           Just
         </h2>
       )}
       <Link href="/">
-        <h1 className="title p-10 md:p-0 text-3xl md:text-8xl font-bold text-baseDark tracking-tighter">
+        <h1 className="title p-10 md:p-0 text-3xl md:text-8xl font-bold text-baseDark text-baseWhite tracking-tighter">
           Unconditional
           <Link href="/about">
             <span className="ml-2 title text-8xl text-accentPurple">.</span>
